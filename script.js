bb7f5d01dddf3f5f2dba937aede5f18cd96b3e13
Mayank7505703 <karansharma90900@gmail.com>
2025-09-13 22:20:25 +0530

    To-Do App commited

---
+++ script.js
@@ -0,0 +1,25 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function Addtask() {
+  if (inputBox.value === '') {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.textContent = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.textContent = "\u00d7";
+    li.appendChild(span);
+
+    span.onclick = function () {
+      li.remove();
+    };
+
+    li.onclick = function () {
+      li.classList.toggle("checked");
+    };
+  }
+  inputBox.value = '';
+}
